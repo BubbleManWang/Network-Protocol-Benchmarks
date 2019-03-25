60aa82c9b6261f75c5f95d35a7fd196db1e58cc2
M. Fatih MAR <mfatihmar@gmail.com>
2019-03-25 23:51:31 +0000

    Implement Proxy Skeleton

--- simulator/proxy/proxy.go
+++ simulator/proxy/proxy.go
@@ -1,14 +1,54 @@
 package proxy
 
-import "errors"
+import (
+	"fmt"
+	"net"
+)
 
 var IsAlive bool
 
+var _mConn *net.UDPConn
+var _xAddr *net.UDPAddr
+var _yAddr *net.UDPAddr
+
 func Initialize(listenPort, leftPort, rightPort int) error {
-	// TODO
-	return errors.New("not implemented")
+	mAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", listenPort))
+	if err != nil {
+		return err
+	}
+
+	xAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", leftPort))
+	if err != nil {
+		return err
+	}
+
+	yAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("localhost:%d", rightPort))
+	if err != nil {
+		return nil
+	}
+
+	mConn, err := net.ListenUDP("udp", mAddr)
+	if err != nil {
+		return err
+	}
+
+	_mConn = mConn
+	_xAddr = xAddr
+	_yAddr = yAddr
+
+	IsAlive = true
+
+	// TODO: send/recv routines
+
+	return nil
 }
 
 func Shutdown() {
-	// TODO
+	if !IsAlive {
+		return
+	}
+
+	IsAlive = false
+
+	_mConn.Close()
 }
