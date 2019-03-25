5864b58967997742bd3f12fe3ba4afc3d50fc1c3
M. Fatih MAR <mfatihmar@gmail.com>
2019-03-25 13:37:33 +0000

    Create Proxy Package

---
+++ simulator/proxy/proxy.go
@@ -0,0 +1,14 @@
+package proxy
+
+import "errors"
+
+var IsAlive bool
+
+func Initialize(listenPort, leftPort, rightPort int) error {
+	// TODO
+	return errors.New("not implemented")
+}
+
+func Shutdown() {
+	// TODO
+}
