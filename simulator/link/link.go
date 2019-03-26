3def1f826c018a01fc2a7a1d64f812b6b77e89fa
M. Fatih MAR <mfatihmar@gmail.com>
2019-03-26 12:37:59 +0000

    Implement Link Skeleton

--- simulator/link/link.go
+++ simulator/link/link.go
@@ -1,7 +1,5 @@
 package link
 
-import "errors"
-
 var IsAlive bool
 
 var XPushCh chan []byte
@@ -9,11 +7,41 @@ var XPullCh chan []byte
 var YPushCh chan []byte
 var YPullCh chan []byte
 
-func Spawn() error {
-	// TODO
-	return errors.New("not implemented")
+var _rateMin, _rateMax int
+var _lossMin, _lossMax int
+var _delayMin, _delayMax int
+
+func Spawn(rateMin, rateMax, lossMin, lossMax, delayMin, delayMax int) error {
+	// TODO: check args
+
+	_rateMin = rateMin
+	_rateMax = rateMax
+	_lossMin = lossMin
+	_lossMax = lossMax
+	_delayMin = delayMin
+	_delayMax = delayMax
+
+	XPushCh = make(chan []byte)
+	XPullCh = make(chan []byte)
+	YPushCh = make(chan []byte)
+	YPullCh = make(chan []byte)
+
+	IsAlive = true
+
+	// TODO: routines
+
+	return nil
 }
 
 func Kill() {
-	// TODO
+	if !IsAlive {
+		return
+	}
+
+	IsAlive = false
+
+	close(XPushCh)
+	close(XPullCh)
+	close(YPushCh)
+	close(YPullCh)
 }
