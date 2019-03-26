c6c371e0a6b2b10b7429fa689bc51a0290b8f68e
M. Fatih MAR <mfatihmar@gmail.com>
2019-03-26 09:21:14 +0000

    Implement Proxy Routing

--- simulator/main.go
+++ simulator/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./proxy"
+)
 
 func main() {
-	fmt.Printf("simulator\n")
+	err := proxy.Initialize(1337, 9696, 6969)
+	if err != nil {
+		fmt.Printf("cannot initialize proxy > %s", err)
+		return
+	}
+
+	for proxy.IsAlive {
+		select {
+		case data := <-proxy.XRecvCh:
+			proxy.YSendCh <- data
+		case data := <-proxy.YRecvCh:
+			proxy.XSendCh <- data
+		}
+	}
 }
