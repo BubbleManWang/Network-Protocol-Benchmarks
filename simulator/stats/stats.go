b84d0373a472b5b7ce771d9f5b55b9b32aaa80ed
M. Fatih MAR <mfatihmar@gmail.com>
2019-03-27 13:27:06 +0000

    Add Tracking Channels to Stats

--- simulator/stats/stats.go
+++ simulator/stats/stats.go
@@ -1,8 +1,23 @@
 package stats
 
-import "errors"
+import (
+	"errors"
+
+	"../core"
+)
 
 var IsAlive bool
+
+var XRecvCh chan *core.Packet
+var XPushCh chan *core.Packet
+var XLossCh chan *core.Packet
+var XPullCh chan *core.Packet
+var XSendCh chan *core.Packet
+var YRecvCh chan *core.Packet
+var YPushCh chan *core.Packet
+var YLossCh chan *core.Packet
+var YPullCh chan *core.Packet
+var YSendCh chan *core.Packet
 
 func Spawn() error {
 	// TODO
