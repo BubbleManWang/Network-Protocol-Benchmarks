912d37ac25d692998e5a471789228bd84b4382f5
M. Fatih MAR <mfatihmar@gmail.com>
2019-04-02 00:52:47 +0100

    Implement Delay Condition

--- simulator/conds/conds.go
+++ simulator/conds/conds.go
@@ -4,8 +4,23 @@ var IsAlive bool
 
 var RateCh, LossCh, DelayCh chan int
 
+var _rateMin, _rateMax int
+var _lossMin, _lossMax int
+var _delayMin, _delayMax int
+
+var _condDelay int64
+
 func Spawn(rateMin, rateMax, lossMin, lossMax, delayMin, delayMax int) error {
 	// TODO: check args?
+
+	_rateMin = rateMin
+	_rateMax = rateMax
+	_lossMin = lossMin
+	_lossMax = lossMax
+	_delayMin = delayMin
+	_delayMax = delayMax
+
+	_condDelay = int64(delayMin)
 
 	RateCh = make(chan int)
 	LossCh = make(chan int)
@@ -19,8 +34,7 @@ func Spawn(rateMin, rateMax, lossMin, lossMax, delayMin, delayMax int) error {
 }
 
 func Delay() int64 {
-	// TODO: delay & jitter
-	return 0
+	return _condDelay
 }
 
 func Loss(packetSize int) bool {
--- simulator/conds/impl.go
+++ simulator/conds/impl.go
@@ -1,11 +1,21 @@
 package conds
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 func update() {
+	rand.Seed(time.Now().UnixNano())
 	for IsAlive {
 		time.Sleep(time.Second)
 
-		// TODO: update modifiers
+		// delay
+		if delayDiff := _delayMax - _delayMin; delayDiff > 0 {
+			_condDelay = int64(_delayMin + rand.Intn(delayDiff))
+			_condDelay *= time.Millisecond.Nanoseconds()
+		}
+
+		// TODO: update _condRate, _condLoss
 	}
 }
