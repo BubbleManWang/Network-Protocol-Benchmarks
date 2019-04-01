54044e0f33b1b3aea49b7465c0a6bd2d4abbbc0f
M. Fatih MAR <mfatihmar@gmail.com>
2019-04-01 13:38:49 +0100

    Implement Conditions Internals

--- simulator/conds/conds.go
+++ simulator/conds/conds.go
@@ -1,14 +1,20 @@
 package conds
 
-import (
-	"../logs"
-)
-
 var IsAlive bool
 
+var RateCh, LossCh, DelayCh chan int
+
 func Spawn(rateMin, rateMax, lossMin, lossMax, delayMin, delayMax int) error {
-	// TODO
-	logs.LogTrace("not implemented yet")
+	// TODO: check args?
+
+	RateCh = make(chan int)
+	LossCh = make(chan int)
+	DelayCh = make(chan int)
+
+	IsAlive = true
+
+	go update()
+
 	return nil
 }
 
@@ -23,5 +29,13 @@ func Loss(packetSize int) bool {
 }
 
 func Kill() {
-	// TODO
+	if !IsAlive {
+		return
+	}
+
+	IsAlive = false
+
+	close(RateCh)
+	close(LossCh)
+	close(DelayCh)
 }
---
+++ simulator/conds/impl.go
@@ -0,0 +1,11 @@
+package conds
+
+import "time"
+
+func update() {
+	for IsAlive {
+		time.Sleep(time.Second)
+
+		// TODO: update modifiers
+	}
+}
